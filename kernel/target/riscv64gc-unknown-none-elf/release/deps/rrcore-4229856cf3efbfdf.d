730e309bf4dfe305bcfaff477995223a07c2237c
unique9 <782638225@qq.com>
2026-04-04 02:37:51 +0800

    remove target/* files and Cargo.lock

--- kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-4229856cf3efbfdf.d
+++
@@ -1,9 +0,0 @@
-/home/unique9/operating-system/rrcore/kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-4229856cf3efbfdf.d: src/main.rs src/lang_item.rs src/console.rs src/sbi.rs src/entry.asm
-
-/home/unique9/operating-system/rrcore/kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-4229856cf3efbfdf: src/main.rs src/lang_item.rs src/console.rs src/sbi.rs src/entry.asm
-
-src/main.rs:
-src/lang_item.rs:
-src/console.rs:
-src/sbi.rs:
-src/entry.asm:
--- kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-7ca78aaaf9646c33.d
+++
@@ -1,9 +0,0 @@
-/home/unique9/operating-system/rrcore/kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-7ca78aaaf9646c33.d: src/main.rs src/lang_item.rs src/console.rs src/sbi.rs src/entry.asm
-
-/home/unique9/operating-system/rrcore/kernel/target/riscv64gc-unknown-none-elf/release/deps/rrcore-7ca78aaaf9646c33: src/main.rs src/lang_item.rs src/console.rs src/sbi.rs src/entry.asm
-
-src/main.rs:
-src/lang_item.rs:
-src/console.rs:
-src/sbi.rs:
-src/entry.asm:
