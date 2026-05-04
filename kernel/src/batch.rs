375f61e95e6fbea7f60170bd6fce2a6e863bc779
unique9 <782638225@qq.com>
2026-05-05 00:49:32 +0800

    add trap.S TrapContext

--- kernel/src/batch.rs
+++ kernel/src/batch.rs
@@ -1,6 +1,39 @@
-//
-//  kernel/src/batch.rs 
-//
+/*
+ *
+ *  kernel/src/batch.rs 
+ *
+ */
+
+const USER_STACK_SIZE: usize =  4096 * 2;
+const KERNEL_STACK_SIZE: usize =  4096 * 2;
+
+#[repr(align(4096))]
+struct UserStack {
+    data: [u8; USER_STACK_SIZE],
+}
+
+impl UserStack {
+    pub fn get_sp(&self) -> usize
+    {
+        self.data.as_ptr() as usize + USER_STACK_SIZE
+    }
+}
+
+#[repr(align(4096))]
+struct KernelStack {
+    data: [u8; KERNEL_STACK_SIZE],
+}
+
+impl KernelStack {
+    pub fn get_sp(&self) -> usize
+    {
+        self.data.as_ptr() as usize + KERNEL_STACK_SIZE
+    }
+}
+
+static USER_STACK = UserStack {data: [0; USER_STACK_SIZE]};
+static KERNEL_STACK = KernelStack {data: [0; USER_STACK_SIZE]};
+
 
 static APP_MANAGER: UPSafeCell<Option<AppManager>> = UPsafeCell::new(None);
 
